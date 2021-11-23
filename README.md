b2e84104b3966d50f8c5b17388989d1ee37f2dca
Shreejan Joshi <shreejanjoshi97@gmail.com>
2021-11-23 09:17:28 +0100

    making my file reuseable and add text in read me

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # CMS-Projects
+
+In this project I'm trying to make CMS of my own. The main reason I'm doing this project is to grow my own knowledge, building something from streatch will help me in long run.
+
+### So What Going On
+
+What I first did is added template from wrapbootstrap for cms projects. I don't want to spend my time on buiding degine in HTML and CSS so I basily skip that.
+After that I made database in phpmyadmin with name CMS and made table category inside CMS.
+Also I will use API called MYSQLI.
--- includes/database.php
+++ includes/database.php
@@ -1,17 +1,18 @@
-<?php 
+<?php
 
+$db['db_host'] = "localhost";
+$db['db_user'] = "root";
+$db['db_password'] = "";
+$db['db_name'] = "cms";
 
-    $db['db_host'] = "localhost";
-    $db['db_user'] = "root";
-    $db['db_password'] = "";
-    $db['db_name'] = "cms";
+foreach ($db as $key => $value) {
+    define(strtoupper($key), $value);
+}
 
-    foreach($db as $key => $value){
-        define(strtoupper($key), $value);
-    }
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
-    $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
-    if(!$connection){
-        echo "Database connection error";
-    }
+if (!$connection) {
+    echo "Database connection error";
+} else {
+    echo "connected";
+}
