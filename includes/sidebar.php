c4c3b7cd6370918122915b34b865caeecd8306e5
Shreejan Joshi <shreejanjoshi97@gmail.com>
2021-11-27 18:36:07 +0100

    categories data is out from database

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -20,42 +20,33 @@
 
     <!-- Blog Categories Well -->
     <div class="well">
+
+        <?php
+        $query = "SELECT * FROM categories";
+        $selectCategoriesSidebar = mysqli_query($connection, $query);
+
+        if (!$selectCategoriesSidebar) {
+            die("Query Error " . mysqli_error($connection));
+        }
+        ?>
         <h4>Blog Categories</h4>
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-12">
                 <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
+
+                    <?php
+                    while ($row = mysqli_fetch_assoc($selectCategoriesSidebar)) {
+                        $catTitle = $row['cat_title'];
+                        echo "<li><a href='#'>{$catTitle}</a></li>";
+                    }
+                    ?>
                 </ul>
             </div>
-            <!-- /.col-lg-6 -->
-            <div class="col-lg-6">
-                <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.col-lg-6 -->
         </div>
         <!-- /.row -->
     </div>
 
     <!-- Side Widget Well -->
-    <div class="well">
-        <h4>Side Widget Well</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-    </div>
+    <?php include "widget.php"; ?>
 
 </div>
